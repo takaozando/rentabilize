0e7612c39c8883e9e8116edf9d1c92549074c587
Gustavo Takao <gustavo_hideo@hotmail.com>
2022-03-12 23:03:47 -0300

    create ytb embedded & more 2 sections

--- new-app/src/pages/Home.js
+++ new-app/src/pages/Home.js
@@ -1,20 +1,29 @@
 import React from "react";
 import logo from './logo.png';
-import './Home.css'
+import './Home.css';
+import YoutubeEmbed from "../components/videoPlayer/YoutubeEmbed";
 
 const HomePage = () => {
     return (
-        <div className="homePage">
-            <header className="headerContainer">
-                <img className="headerLogo" src={logo} alt="logo_mg"></img>
-                <p>Rentabilize</p>
-            </header>
-            <body>
-                <section className="videoContainer">
-                    <video className="videoContent" src={'https://www.youtube.com/watch?v=5qap5aO4i9A'} width="600" height="300" controls="controls" autoplay="true" />
-                </section>
-            </body>
-        </div>
+        <main className="homePage">
+            <section className="firstSection">
+                <header className="headerContainer">
+                    <img className="headerLogo" src={logo} alt="logo_img"></img>
+                    <h1>Rentabilize</h1>
+                </header>
+                <div className="videoContainer">
+                    <YoutubeEmbed embedId="rokGy0huYEA" />
+                    </div>
+            </section>
+            <section className="secondSection">
+                O que você irá aprender:
+            </section>
+            <section className="thirdSection">
+                Quem sou eu
+            </section>
+
+
+        </main>
     )
 }
 export default HomePage;
